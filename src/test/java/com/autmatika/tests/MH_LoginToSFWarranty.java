e5197cb1463839fd056b1cb9331999f7fd538921
John Doe <john@sample.com>
2018-09-07 10:14:16 -0500

    Added Test Files for Meritage Warranty.

---
+++ src/test/java/com/autmatika/tests/MH_LoginToSFWarranty.java
@@ -0,0 +1,17 @@
+package com.autmatika.tests;
+
+import com.autmatika.tests.withPages.theInternet.HomePage;
+import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utopia.sphnx.dataconversion.datagen.generator.DataGenerator;
+
+import static com.codeborne.selenide.Condition.disappears;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MH_LoginToSFWarranty {
+
+
+}
