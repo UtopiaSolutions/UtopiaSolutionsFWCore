c3ee869b44fb6db87d6ab3d107edd07d286faebc
JeffreyHeytow <heytow1@gmail.com>
2019-05-30 13:07:52 -0500

    added test resources and test class

---
+++ src/test/java/CoreTest.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+public class CoreTest {
+    @BeforeTest
+    public void setupTest() {
+        System.out.println("@BeforeTest");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("@BeforeMethod");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("AfterMethod");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("@AfterTest");
+    }
+}
