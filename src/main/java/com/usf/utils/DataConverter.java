960b120d767ff9b3d34011ff25f7df89cbec785f
JeffreyHeytow <heytow1@gmail.com>
2019-06-10 12:08:25 -0500

    Added dummy class for mock up demo

---
+++ src/main/java/com/usf/utils/DataConverter.java
@@ -0,0 +1,12 @@
+package com.usf.utils;
+
+/** THIS IS A DUMMY CLASS USED FOR DEMO PURPOSES --
+ * IT IS USED FOR MOCK-UPs */
+public class DataConverter {
+
+    public static String getMetadataValue(String key) {
+        return key;
+    }
+
+
+}
