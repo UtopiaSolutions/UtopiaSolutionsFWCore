2b2ceebe478f2fa6e48ec54ed0b88679397f3620
JeffreyHeytow <heytow1@gmail.com>
2019-06-27 08:26:12 -0500

    cleared new feature test

--- src/main/java/test/NewFeatureTest.java
+++ src/main/java/test/NewFeatureTest.java
@@ -1,14 +1,12 @@
 package test;
 
-import com.usf.utils.ConfigurationReader;
 import org.testng.annotations.Test;
 
 public class NewFeatureTest {
 
     @Test
     public static void featureTest() {
-        ConfigurationReader config = new ConfigurationReader("config.properties");
-        System.out.println(config.getConfigValue("testval"));
+
 
 
     }
