3d5ca124f41b619f70390fd8e87c5dea64cd45d6
JeffreyHeytow <heytow1@gmail.com>
2019-07-16 11:16:25 -0500

    fixed logging

--- src/main/java/test/NewFeatureTest.java
+++ src/main/java/test/NewFeatureTest.java
@@ -1,13 +1,6 @@
 package test;
 
-import com.usf.metadata.Metadata;
-import com.usf.utils.parsers.Parser;
 import org.testng.annotations.Test;
-
-import java.util.Iterator;
-import java.util.Map;
-
-import static com.usf.utils.ConfigurationReader.getConfigValue;
 
 /**
  * This class is FOR UNIT TESTING ONLY -
@@ -18,17 +11,9 @@ public class NewFeatureTest extends BaseUITest {
 
     @Test
     public static void featureTest() {
-        Parser parser = new Parser(getConfigValue("metadata-path"), "metadata");
 
-        parser.parseFile();
-
-
-        Iterator it = Metadata.getInstance().getMetadata().entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            System.out.println(pair.getKey() + " = " + pair.getValue());
-            it.remove(); // avoids a ConcurrentModificationException
-        }
-
+        System.out.println("BOOM");
     }
+
+
 }
