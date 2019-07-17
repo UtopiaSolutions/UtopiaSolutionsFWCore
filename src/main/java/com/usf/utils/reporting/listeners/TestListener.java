6d1fe826bdea03dca52851ba371401e832f89f64
JeffreyHeytow <heytow1@gmail.com>
2019-07-17 14:45:23 -0500

    screenshot path fix

--- src/main/java/com/usf/utils/reporting/listeners/TestListener.java
+++ src/main/java/com/usf/utils/reporting/listeners/TestListener.java
@@ -1,15 +1,14 @@
 package com.usf.utils.reporting.listeners;
 
-import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.usf.utils.reporting.ExtentManager;
 import com.usf.utils.reporting.ExtentTestManager;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.io.FileHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
@@ -18,9 +17,9 @@ import org.testng.ITestResult;
 import test.BaseUITest;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -58,60 +57,14 @@ public class TestListener extends BaseUITest implements ITestListener {
         ExtentTestManager.getTest().log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
         ExtentTestManager.getTest().log(Status.FAIL, MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
         WebDriver driver = getWebDriver();
-//        try {
-//            //Take base64Screenshot screenshot.
-//            String base64Screenshot = "data:image/png;base64," + ((TakesScreenshot) webDriver).
-//                    getScreenshotAs(OutputType.BASE64);
-//            ExtentTestManager.getTest().log(Status.FAIL, "Click for image details --> ",
-//                    MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
-//        } catch (IOException e) {
-//            log.error("Screenshot could not be captured.");
-//            ExtentTestManager.getTest().fail("Screenshot could not be captured.");
-//        }
-        String targetLocation = null;
-        Date date = new Date();
-        Timestamp timeStamp = new Timestamp(date.getTime());
-
-        String testClassName = result.getClass().toString();
-        String testMethodName = result.getName().trim();
-        String screenShotName = testMethodName + "_" + timeStamp + ".png";
-        String FILE_SEPARATOR = System.getProperty("file.separator");
-        String FOLDER_PATH = System.getProperty("user.dir") + FILE_SEPARATOR + "reports" + FILE_SEPARATOR + "screenshots";
-        log.info("Screen shots reports path - " + FOLDER_PATH);
-
-
         try {
-            File file = new File(FOLDER_PATH + FILE_SEPARATOR + testClassName); // Set screenshots folder
-            if (!file.exists()) {
-                if (file.mkdirs()) {
-                    log.info("Directory: " + file.getAbsolutePath() + " is created!");
-                } else {
-                    log.info("Failed to create directory: " + file.getAbsolutePath());
-                }
-
-            }
-
-            File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            targetLocation = FOLDER_PATH + FILE_SEPARATOR + testClassName + FILE_SEPARATOR + screenShotName;// define location
-            File targetFile = new File(targetLocation);
-            log.info("Screen shot file location - " + screenshotFile.getAbsolutePath());
-            log.info("Target File location - " + targetFile.getAbsolutePath());
-            FileHandler.copy(screenshotFile, targetFile);
-
-        } catch (FileNotFoundException e) {
-            log.error("File not found exception occurred while taking screenshot " + e.getMessage());
-        } catch (Exception e) {
-            log.error("An exception occurred while taking screenshot " + e.getCause());
-        }
-
-        // attach screenshots to report
-        try {
-            ExtentTestManager.getTest().fail("Screenshot",
-                    MediaEntityBuilder.createScreenCaptureFromPath(targetLocation).build());
+            String screenshot = captureScreen(driver, generateFileName(result));
+            ExtentTestManager.getTest().fail("Screenshot: " + ExtentTestManager.getTest().addScreenCaptureFromPath(screenshot));
         } catch (IOException e) {
-            log.error("An exception occured while taking screenshot " + e.getCause());
+            ExtentTestManager.getTest().fail("Could not capture screenshot.");
+            log.error("Could not capture screenshot.");
         }
-        ExtentTestManager.getTest().log(Status.FAIL, "Test Failed");
+
     }
 
     @Override
@@ -123,6 +76,29 @@ public class TestListener extends BaseUITest implements ITestListener {
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
         log.info("*** Test failed but within percentage % " + result.getMethod().getMethodName());
+    }
+
+    public static String captureScreen(WebDriver driver, String screenName) throws IOException{
+
+        TakesScreenshot screen = (TakesScreenshot) driver;
+        File src = screen.getScreenshotAs(OutputType.FILE);
+        String FILE_SEPARATOR = System.getProperty("file.separator");
+        String dest = System.getProperty("user.dir") + FILE_SEPARATOR
+                + "reports" + FILE_SEPARATOR
+                + "screenshots" + FILE_SEPARATOR
+                + screenName + ".png";
+        File target = new File(dest);
+        FileUtils.copyFile(src, target);
+
+        return dest;
+    }
+
+    public static String generateFileName(ITestResult result){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd SSS");
+        Date date = new Date();
+        String fileName = result.getName()+ "_" + dateFormat.format(date);
+        return fileName;
+
     }
 
 }
